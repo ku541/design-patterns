8a5f24bc80fe6f2ccc5db15003fe96943bd03955
Kusal Wettimuny <ku541w@gmail.com>
2024-09-22 16:57:03 +0530

    fix: remove comments

--- index.php
+++ index.php
@@ -2,20 +2,6 @@
 
 require_once 'vendor/autoload.php';
 
-// use Kusal\DesignPatterns\Behavioral\Command\TV;
-// use Kusal\DesignPatterns\Behavioral\Command\On;
-// use Kusal\DesignPatterns\Behavioral\Command\Remote;
-
-// $tv = new TV;
-
-// $on = new On($tv);
-
-// $remote = new Remote();
-
-// $remote->send($on);
-
-// var_dump($tv);
-
 switch ($argv[1]) {
     case 'command':
         require_once 'src/behavioral/command/index.php';
